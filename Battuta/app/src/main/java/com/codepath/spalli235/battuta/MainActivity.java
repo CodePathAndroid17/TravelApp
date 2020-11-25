bb84fbd6994f916f01146cf7a95c665a7012c3e0
spalli235 <49597499+spalli235@users.noreply.github.com>
2020-11-24 20:45:34 -0500

    Initial project upload

---
+++ Battuta/app/src/main/java/com/codepath/spalli235/battuta/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codepath.spalli235.battuta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
